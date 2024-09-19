5fd9e162d6341e9b41613f2080044bec3e1cbbd1
ATsonev <alexconev1@gmail.com>
2024-09-19 16:10:20 +0300

    Added rest controller for bookie

--- src/main/java/com/example/astrologyapp/config/SecurityConfig.java
+++ src/main/java/com/example/astrologyapp/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                                 authorizeRequests
                                         .requestMatchers("/images/**", "/css/**", "/js/**").permitAll()
                                         .requestMatchers("/", "/login", "/register", "/error",
-                                                "/appointment", "/contacts", "/appointments", "/api/clients").permitAll()
+                                                "/appointment", "/contacts", "/appointments", "/api/users").permitAll()
                                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
